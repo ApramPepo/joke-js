6b0a088cf0d54aee3ee793092511bed9e56feba2
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-09-03 00:02:41 +0300

    working app

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const joke = document.querySelector(".joke");
 const jokeBtn = document.querySelector(".jokeBtn");
-const url = ""
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 
-function randomJoke() {
-    fetch
+async function randomJoke() {
+    fetch(url).then((resp) => resp.json())
+
+    .then((data) => {
+        joke.innerHTML = data.joke;
+    })
+
 }
+
+
+randomJoke();
+
+jokeBtn.addEventListener("click", ()=>{
+    randomJoke();
+});
